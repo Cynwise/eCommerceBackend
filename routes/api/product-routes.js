aa8aa3c544daf91925d46d0dc1d6dd83921fdeb0
William Farnsworth <williamcoreyf@gmail.com>
2021-06-17 09:24:09 -0400

    completed product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,36 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category}],
+      include: [{model: Tag}]
+    })
+    res.json(productData);
+  } catch (err) {
+    res.json(err);
+  };
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}],
+      include: [{model: Tag}]
+    });
+
+    if(!productData) {
+      res.status(404).json({message: 'No product with given id'});
+      return;
+    };
+
+    res.json(productData);
+  } catch (err){
+    res.status.json(err);
+  };
 });
 
 // create new product
